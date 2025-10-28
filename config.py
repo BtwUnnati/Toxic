2eb06abeaef6515bab55a480921a6e60e23af902
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <222322462+BtwUnnati@users.noreply.github.com>
2025-10-28 23:41:56 +0530

    Update OWNER_ID to a static value

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600000000000000))
 LOGGER_ID = -1002843633996
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = getenv("OWNER_ID" "8233966309")
+OWNER_ID = 8233966309
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
