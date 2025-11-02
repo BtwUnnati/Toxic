942953e069277130ca185ff7374a595fe4c83591
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <222322462+BtwUnnati@users.noreply.github.com>
2025-11-02 20:40:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600000000000000))
 LOGGER_ID = -1003199377375
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = 8233966309
+OWNER_ID = 8294942940
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
